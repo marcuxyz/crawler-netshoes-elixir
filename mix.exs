8cdf43fa4071cbb611cf9a7e60d97e57c999e885
Marcus Pereira <mpereirassa@gmail.com>
2023-04-18 10:10:08 -0300

    🚀 feat(mix.exs): adiciona dependência httpoison Adiciona a dependência httpoison ao projeto Dwarf. Essa dependência será usada para fazer solicitações HTTP em outras APIs.

--- mix.exs
+++ mix.exs
@@ -21,10 +21,8 @@ defmodule Dwarf.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test},
       {:httpoison, "~> 2.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
