75eb930004241ee40e84638f9e1e5ed43e289fa5
Marcus Pereira <mpereirassa@gmail.com>
2023-04-18 10:05:50 -0300

    Init application

---
+++ lib/dwarf.ex
@@ -0,0 +1,18 @@
+defmodule Dwarf do
+  @moduledoc """
+  Documentation for `Dwarf`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dwarf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
