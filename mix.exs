12e9e410af3afd0a19517c55868b8e7214b84405
Marcus Pereira <mpereirassa@gmail.com>
2023-04-22 08:25:04 -0300

    🔧 chore: adiciona configurações de compilação e dependências Adiciona as configurações de compilação elixirc_options e elixirc_paths para melhorar a qualidade do código e evitar erros. As opções de compilação são definidas para tratar avisos como erros. As dependências adicionadas são a biblioteca de serialização JSON Poison e a biblioteca de web scraping Floki. A biblioteca Floki é usada para testes de integração e a biblioteca Poison é usada para serializar e desserializar JSON.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Dwarf.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_options: [warnings_as_errors: true],
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
@@ -18,10 +20,16 @@ defmodule Dwarf.MixProject do
     ]
   end
 
+  # ensure test/support is compiled
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:mox, "~> 1.0", only: :test},
+      {:poison, "~> 5.0"},
+      {:floki, "~> 0.34.0"},
       {:httpoison, "~> 2.0"}
     ]
   end
