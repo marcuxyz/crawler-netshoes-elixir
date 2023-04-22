e8f5d46e681ff5b35f2768300c931ca3387d0e16
Marcus Pereira <mpereirassa@gmail.com>
2023-04-22 09:00:03 -0300

    ✨ feat(netshoes.ex): adiciona extração de preço original e preço ✅ test(dwarf_test.exs): adiciona teste para extração de preço original e preço Adiciona a extração de preço original e preço do HTML da página do produto da NetShoes. Isso permite que o aplicativo obtenha informações mais detalhadas sobre o produto. Também adiciona um teste para verificar se a extração de preço original e preço está funcionando corretamente.

--- test/dwarf_test.exs
+++ test/dwarf_test.exs
@@ -17,6 +17,10 @@ defmodule DwarfTest do
        }}
     end)
 
-    assert NetShoes.get(slug) == %{title: "Tênis Fila Racer Carbon Masculino - Branco+Vermelho"}
+    assert NetShoes.get(slug) == %{
+             title: "Tênis Fila Racer Carbon Masculino - Branco+Vermelho",
+             original_price: "R$ 999,90",
+             price: "R$ 699,99"
+           }
   end
 end
